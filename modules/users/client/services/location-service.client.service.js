8e358b561cf1c660f2ccdb8f8c541dcc741729b0
LucasBento <lucas.bsilva@outlook.com>
2016-02-20 13:38:29 -0200

    Implemented location service

---
+++ modules/users/client/services/location-service.client.service.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('users')
+    .factory('locationService', locationService);
+
+  locationService.$inject = ['$window'];
+
+  function locationService($window) {
+    function getReverseGeocodedLocation(location) {
+      var geocoder = new google.maps.Geocoder();
+      var coordinates = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+
+      var response = 'Address not availabe';
+      geocoder.geocode({ 'latLng': coordinates }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            return results[0].formatted_address;
+          }
+        }
+      });
+    }
+
+    function getUserLocation() {
+      $window.navigator.geolocation.getCurrentPosition(
+        function (location) {
+          return {
+            code: 1,
+            location: {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude
+            },
+            address: getReverseGeocodedLocation(location)
+          };
+        },
+        function (err) {
+          return { code: 0, message: 'User location not available.' };
+        }
+      );
+    }
+
+    return {
+      getUserLocation: getUserLocation
+    };
+  }
+})();
