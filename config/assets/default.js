5b5bdf78de8ca97ce30384df1d71c99b5ff59e95
LucasBento <lucas.bsilva@outlook.com>
2016-02-20 14:48:53 -0200

    Starting to use ng-crop to crop images in profile

--- config/assets/default.js
+++ config/assets/default.js
@@ -16,7 +16,9 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/ngImgCrop/source/js/init.js',
+        'public/lib/ngImgCrop/source/js/ng-img-crop.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
@@ -27,7 +29,8 @@ module.exports = {
       'modules/*/client/less/*.less'
     ],
     sass: [
-      'modules/*/client/scss/*.scss'
+      'modules/*/client/scss/*.scss',
+      'public/lib/ngImgCrop/source/scss/ng-img-crop.scss'
     ],
     js: [
       'modules/core/client/app/config.js',
