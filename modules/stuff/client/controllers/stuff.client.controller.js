1b19732e356fb805c4dbfd0d25d65028aabd2589
LucasBento <lucas.bsilva@outlook.com>
2016-02-21 13:41:28 -0300

    Stuff crud

--- modules/stuff/client/controllers/stuff.client.controller.js
+++ modules/stuff/client/controllers/stuff.client.controller.js
@@ -5,19 +5,38 @@
     .module('stuff')
     .controller('StuffController', StuffController);
 
-  StuffController.$inject = ['stuffService'];
+  StuffController.$inject = ['stuffService', '$http'];
 
-  function StuffController(stuffService) {
+  function StuffController(stuffService, $http) {
     var vm = this;
 
-    getStuff('test');
+    vm.getStuff = getStuff;
 
     function getStuff(value) {
-      var stuff = stuffService.findStuff(value, );
-
-      stuff.success(function(response) {
-        vm.list = response;
+      // var stuff = stuffService.findStuff(value);
+return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: value,
+        sensor: false
+      }
+    }).then(function(response){
+      return response.data.results.map(function(item){
+        return item.formatted_address;
       });
-    };
+    });
+      // stuff.error(function(response) {
+      //   response = [{
+      //     "name": "Cooking",
+      //     "slug": "cooking",
+      //     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tristique ultricies felis, sed tristique nisi rhoncus a.",
+      //     "type": 4,
+      //     "matchType": 6
+      //   }]
+
+      //   return response.data.results.map(function(item){
+      //     return item.name;
+      //   });
+      // });
+    }
   }
 })();
