c847fc04a732fd6b1e81e84e4fd3cb377e624c4b
Mustapha Babatunde <bmustapha@c2gconsulting.com>
2016-02-21 15:48:53 +0100

    Complete profile view mockup

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -4,7 +4,36 @@ angular.module('users').controller('EditProfileController', ['$scope', '$http', 
   function ($scope, $http, $location, Users, Authentication) {
     $scope.user = Authentication.user;
 
+
+    /*
+    * Mock logic
+    * */
     $scope.range = 32;
+    $http.get('/api/users/stuff').then(function (response) {
+      console.log(response.data);
+      $scope.stuff = response.data;
+    }, function (error) {
+      console.log(error);
+    });
+
+    var give = 1,
+      receive = 2,
+      collaborate = 4;
+
+    $scope.checkGive = function (value) {
+      return value & give;
+    };
+    $scope.checkReceive = function (value) {
+      return value & receive;
+    };
+    $scope.checkCollaborate = function (value) {
+      return value & collaborate;
+    };
+
+    /*
+    * End of mock logic
+    * */
+
 
     // Update a user profile
     $scope.updateUserProfile = function (isValid) {
