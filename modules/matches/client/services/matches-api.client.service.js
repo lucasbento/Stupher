067dd612b49dd1fa94b314020d2db20e30eecfa5
LucasBento <lucas.bsilva@outlook.com>
2016-02-21 12:26:40 -0300

    Refactored matches and stuff api

--- modules/matches/client/services/matches-api.client.service.js
+++ modules/matches/client/services/matches-api.client.service.js
@@ -19,63 +19,44 @@
         postParams.push({ message: message });
       }
 
-      $http.post('/api/users/matches/' + userId + '/' + definition, postParams).success(function(response) {
-        return { code: 1, response: response };
-      }).error(function(err) {
-        return { code: 0, error: err };
-      });
+      return $http.post('/api/users/matches/' + userId + '/' + definition, postParams);
     }
 
     /*
      * Returns match by user_id
      * @param int userId ID of the user
-     * @return array
      */
     function getMatch(userId) {
       if (angular.isUndefined(userId)) {
         return { code: 0, message: 'Must provide userId' };
       }
 
-      $http.get('/api/users/matches/' + userId).success(function(response) {
-        return { code: 1, response: response };
-      }).error(function(err) {
-        return { code: 0, error: err };
-      });
+      return $http.get('/api/users/matches/' + userId);
     }
 
     /*
      * Returns a list of matches
      * @param int skip Number of matches to skip
-     * @return array
      */
     function getMatches(skip) {
       if (angular.isUndefined(skip)) {
         skip = 0;
       }
 
-      $http.get('/api/users/matches/' + skip).success(function(response) {
-        return { code: 1, response: response };
-      }).error(function(err) {
-        return { code: 0, error: err };
-      });
+      return $http.get('/api/users/matches/' + skip);
     }
 
     /*
      * Returns a list of matches,  A match is considered new if the current
      * user is invitee in this match and has not accepted or declined it
      * @param int skip Number of matches to skip
-     * @return array
      */
     function getNewMatches(skip) {
       if (angular.isUndefined(skip)) {
         skip = 0;
       }
 
-      $http.get('/api/users/matches/new/' + skip).success(function(response) {
-        return { code: 1, response: response };
-      }).error(function(err) {
-        return { code: 0, error: err };
-      });
+      return $http.get('/api/users/matches/new/' + skip);
     }
 
     /*
@@ -83,11 +64,7 @@
      * @param int userId ID of the matched user
      */
     function deleteMatch(userId) {
-      $http.delete('/api/users/matches/' + userId).success(function(response) {
-        return { code: 1, response: response };
-      }).error(function(err) {
-        return { code: 0, error: err };
-      });
+      return $http.delete('/api/users/matches/' + userId);
     }
 
     return {
