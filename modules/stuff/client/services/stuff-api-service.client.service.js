2f40d20fef1df1561394cdc5b50e643e67d40e05
LucasBento <lucas.bsilva@outlook.com>
2016-02-21 12:04:47 -0300

    Refactored stuff api service

--- modules/stuff/client/services/stuff-api-service.client.service.js
+++ modules/stuff/client/services/stuff-api-service.client.service.js
@@ -11,72 +11,47 @@
 		/*
 		* Get currently logged in user's stuff
 		* @param string param Can be either a string for slug or an integer for skip
-		* @return array
 		*/
 		function getStuff(param) {
 			if (angular.isUndefined(param))
 				param = '';
 
-			$http.get('/api/users/stuff/' + param).success(function(response) {
-				return { code: 1, response: response };
-			}).error(function(err) {
-				return { code: 0, error: err };
-			});
+			return $http.get('/api/users/stuff/' + param);
 		}
 
 		/*
 		* Add new stuff to currently logged in user's stuff
 		* @param array stuff Stuff parameters
 		* @param string slug
-		* @return array
 		*/
 		function addStuff(stuff) {
-			$http.post('/api/users/stuff', stuff).success(function(response) {
-				return { code: 1, response: response };
-			}).error(function(err) {
-				return { code: 0, error: err };
-			});
+			return $http.post('/api/users/stuff', stuff);
 		}
 
 		/*
 		* Update details of user's stuff (trait) by its slug
 		* @param string slug Slug of user's stuff
 		* @param array params User's stuff data
-		* @return array
 		*/
 		function updateStuff(slug, params) {
-			$http.put('/api/users/stuff/' + slug, params).success(function(response) {
-				return { code: 1, response: response };
-			}).error(function(err) {
-				return { code: 0, error: err };
-			});
+			return $http.put('/api/users/stuff/' + slug, params);
 		}
 
 		/*
 		* Remove user's stuff by its slug
 		* @param string slug Slug of user's stuff
-		* @return array
 		*/
 		function deleteStuff(slug) {
-			$http.delete('/api/users/stuff/' + slug).success(function(response) {
-				return { code: 1, response: response };
-			}).error(function(err) {
-				return { code: 0, error: err };
-			});
+			return $http.delete('/api/users/stuff/' + slug);
 		}
 
 		/*
 		* Stuff autocomplete: find stuff by partial/complete name
 		* @param string name Partial/complete name of stuff
 		* @param array userData User preferences, location and search radius
-		* @return array
 		*/
 		function findStuff(name, userData) {
-			$http.post('/api/stuff/' + name, userData).success(function(response) {
-				return { code: 1, response: response };
-			}).error(function(err) {
-				return { code: 0, error: err };
-			});
+			return $http.post('/api/stuff/' + name, userData);
 		}
 
 		return {
