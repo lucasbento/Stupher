a1877d74aa5aa054f663e1b6def810fb1947d8a2
LucasBento <lucas.bsilva@outlook.com>
2016-02-21 12:19:53 -0300

    Stuff crud

--- modules/stuff/client/controllers/stuff.client.controller.js
+++ modules/stuff/client/controllers/stuff.client.controller.js
@@ -5,9 +5,19 @@
     .module('stuff')
     .controller('StuffController', StuffController);
 
-  // StuffController.$inject = [''];
+  StuffController.$inject = ['stuffService'];
 
-  function StuffController() {
+  function StuffController(stuffService) {
     var vm = this;
+
+    getStuff('test');
+
+    function getStuff(value) {
+      var stuff = stuffService.findStuff(value, );
+
+      stuff.success(function(response) {
+        vm.list = response;
+      });
+    };
   }
 })();
